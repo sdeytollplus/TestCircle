e7682c14307e856c54818b6f0b0cc5c75b31243d
america_dev <america_dev@outlook.com>
2019-02-11 19:00:22 +0530

    first commit

--- tools/test.ps1
+++ tools/test.ps1
@@ -2,4 +2,10 @@ Clear-Host
 $Host
 
 Install-Module -Name Az -AllowClobber -force
+
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+Install-Module AzureRM
+Install-Module Azure
+
 Get-AzureRmSubscription
