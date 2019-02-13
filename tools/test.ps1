18b25ba9cf33c4f7c4425bc60c8111065e6754c3
sdeytollplus <43905431+sdeytollplus@users.noreply.github.com>
2019-02-13 19:52:03 +0530

    Update test.ps1

--- tools/test.ps1
+++ tools/test.ps1
@@ -7,12 +7,14 @@ Import-Module AzureRM.NetCore.Preview
 Invoke-AzureRmResourceAction  -ResourceGroupName $resourceGroupName -ResourceType 'Microsoft.ApiManagement/service/users' -Action 'token' -ResourceName "$serviceName/$userId" -ApiVersion "2016-10-10" -Parameters $parameters
 
      Import-Module AzureRM.NetCore.Preview 
- get-childitem
-          
+     
+     
+# get-childitem
+  import-AzureRmcontext -Path "/root/project/tools/profile1.json"        
    
  # Login-AzureRMAccount 
-$pwUrlencodedLowerCase = [System.Web.HttpUtility]::UrlEncode($pw.value)
+#$pwUrlencodedLowerCase = [System.Web.HttpUtility]::UrlEncode($pw.value)
 
 # Seems like password has to be URL-encoded with UPPERCASE heximal digits (e.g. %3D instead of %3d) for Git to authenticate properly:
-[regex]$regex='(%[0-9a-f][0-9a-f])'
-$pwUrlencoded = $regex.Replace($pwUrlencodedLowerCase, {$args[0].Value.ToUpper()})
+#[regex]$regex='(%[0-9a-f][0-9a-f])'
+#$pwUrlencoded = $regex.Replace($pwUrlencodedLowerCase, {$args[0].Value.ToUpper()})
