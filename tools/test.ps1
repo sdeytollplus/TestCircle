6cab1e89eb612f859303ecd2e0b29d615803f2e5
sdeytollplus <43905431+sdeytollplus@users.noreply.github.com>
2019-02-13 22:50:39 +0530

    Update test.ps1

--- tools/test.ps1
+++ tools/test.ps1
@@ -4,6 +4,9 @@ Import-Module AzureRM.NetCore.Preview
 Import-Module AzureRM.Profile.NetCore.Preview
 Import-Module AzureRM.Resources.NetCore.Preview
 
+                       
+
+
 #Clear-Host
 #Install-Module -Name AzureRM -RequiredVersion 2.4.0 -Repository PSGallery -Force
 #Invoke-AzureRmResourceAction  -ResourceGroupName $resourceGroupName -ResourceType 'Microsoft.ApiManagement/service/users' -Action 'token' -ResourceName "$serviceName/$userId" -ApiVersion "2016-10-10" -Parameters $parameters
@@ -13,13 +16,13 @@ Import-Module AzureRM.Resources.NetCore.Preview
      
      
 
-  #Select-AzureRmProfile -Path '/root/project/tools/profile.json' 
+  Select-AzureRmProfile -Path '/root/project/tools/profile.json' 
   $azureAccountName ="sdey@tollplus.com"
 $azurePassword = ConvertTo-SecureString "satyam1$2$3" -AsPlainText -Force
 
 $psCred = New-Object System.Management.Automation.PSCredential($azureAccountName, $azurePassword)
 
-Login-AzureRmAccount -Credential $psCred
+#Login-AzureRmAccount -Credential $psCred
    
  # Login-AzureRMAccount 
 #$pwUrlencodedLowerCase = [System.Web.HttpUtility]::UrlEncode($pw.value)
