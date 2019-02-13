aaab950684626d7cc52aa0b98774fab13b8cbd71
sdeytollplus <43905431+sdeytollplus@users.noreply.github.com>
2019-02-13 19:03:10 +0530

    Update test.ps1

--- tools/test.ps1
+++ tools/test.ps1
@@ -3,6 +3,8 @@ $Host
 
 #Install-Module -Name AzureRM -RequiredVersion 2.4.0 -Repository PSGallery -Force
 
-get-command *azure*
+$pwUrlencodedLowerCase = [System.Web.HttpUtility]::UrlEncode($pw.value)
 
-Get-AzureRmApiManagementTenantGitAccess
+# Seems like password has to be URL-encoded with UPPERCASE heximal digits (e.g. %3D instead of %3d) for Git to authenticate properly:
+[regex]$regex='(%[0-9a-f][0-9a-f])'
+$pwUrlencoded = $regex.Replace($pwUrlencodedLowerCase, {$args[0].Value.ToUpper()})
