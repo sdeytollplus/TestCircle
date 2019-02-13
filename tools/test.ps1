b554ce5a3ce50183811f2fb99bc87064d7b673e2
sdeytollplus <43905431+sdeytollplus@users.noreply.github.com>
2019-02-13 19:40:30 +0530

    Update test.ps1

--- tools/test.ps1
+++ tools/test.ps1
@@ -1,13 +1,15 @@
 Clear-Host
 $Host
+Import-Module AzureRM.NetCore.Preview 
+
 
 #Install-Module -Name AzureRM -RequiredVersion 2.4.0 -Repository PSGallery -Force
-# Invoke-AzureRmResourceAction  -ResourceGroupName $resourceGroupName -ResourceType 'Microsoft.ApiManagement/service/users' -Action 'token' -ResourceName "$serviceName/$userId" -ApiVersion "2016-10-10" -Parameters $parameters
+Invoke-AzureRmResourceAction  -ResourceGroupName $resourceGroupName -ResourceType 'Microsoft.ApiManagement/service/users' -Action 'token' -ResourceName "$serviceName/$userId" -ApiVersion "2016-10-10" -Parameters $parameters
 
      Import-Module AzureRM.NetCore.Preview 
           
    
-  Login-AzureRMAccount 
+ # Login-AzureRMAccount 
 $pwUrlencodedLowerCase = [System.Web.HttpUtility]::UrlEncode($pw.value)
 
 # Seems like password has to be URL-encoded with UPPERCASE heximal digits (e.g. %3D instead of %3d) for Git to authenticate properly:
